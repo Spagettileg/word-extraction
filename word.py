677aa3d2306335dbb1ae1767ed8700279f48c699
Ubuntu <ubuntu@ip-172-31-40-170.eu-west-1.compute.internal>
2020-12-11 21:19:04 +0000

    User comments added for improved UI.

---
+++ word.py
@@ -0,0 +1,36 @@
+import collections
+import pandas as pd
+
+file = open('doc1.txt', encoding="utf8")
+a= file.read()
+
+stopwords = set(line.strip() for line in open('stopwords.txt'))
+stopwords = stopwords.union(set(['mr','mrs','one','two','said']))
+
+wordcount = {}
+
+for word in a.lower().split():
+    word = word.replace(".","")
+    word = word.replace(",","")
+    word = word.replace(":","")
+    word = word.replace("\"","")
+    word = word.replace("!","")
+    word = word.replace("â€œ","")
+    word = word.replace("â€˜","")
+    word = word.replace("*","")
+    if word not in stopwords:
+        if word not in wordcount:
+            wordcount[word] = 1
+        else:
+            wordcount[word] += 1
+
+# Print most common word
+n_print = int(input("How many most common words to print: "))
+print("\nOK. The {} most common words are as follows\n".format(n_print))
+word_counter = collections.Counter(wordcount)
+for word, count in word_counter.most_common(n_print):
+    print(word, ": ", count)
+
+# Close the file
+file.close()
+
