a7d2019de39358f3327ca110e744b82e64980af0
Ubuntu <ubuntu@ip-172-31-41-97.eu-west-1.compute.internal>
2020-12-12 22:21:32 +0000

    import csv library added.

--- word.py
+++ word.py
@@ -6,11 +6,13 @@ import csv
 path = "data/"
 all_files = os.listdir(path)
 
+# Word extraction from .csv files in data directory
+
 for story in all_files:
    # open the file and then call .read() to get the text
    with open(os.path.join(path, story),"rt") as f:
 
-        text = f.read()
+           text = f.read()
 
 stopwords = set(line.strip() for line in open('data/stopwords.csv'))
 
@@ -26,9 +28,7 @@ for word in text.lower().split():
     word = word.replace("\'","")
     word = word.replace("!","")
     word = word.replace("?","")
-    word = word.replace("-","")
     word = word.replace("$","")
-    word = word.replace("*","")
     if word not in stopwords:
         if word not in wordcount:
             wordcount[word] = 1
@@ -36,18 +36,17 @@ for word in text.lower().split():
             wordcount[word] += 1
 
 # Print most common word
-n_print = int(input("How many most common words to print: "))
-print("\nOK. The {} most common words are as follows\n".format(n_print))
+
 word_counter = collections.Counter(wordcount)
-for word, count in word_counter.most_common(n_print):
+for word, count in word_counter.most_common(10):
     print(word, ": ", count)
+    
 
 # Close the file
 f.close()
 
-
 # Count files in data directory
-list = os.listdir('data/') # data/ is your directory path
+list = os.listdir('data/') # data/ is the directory path
 number_files = len(list)
 print(number_files, ": files in data directory")
 
